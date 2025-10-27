d0f3915789390b2c0e5a2e69f9f908de67996910
Phillip Kuhrt <mail@phi1010.com>
2025-10-27 17:40:22 +0100

    Useful link, sample env and prod install script

---
+++ prod-install.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+set -euxo pipefail
+. ./.env.sh
+ANSIBLE_STDOUT_CALLBACK=debug \
+  uv run ansible-playbook \
+  -i inventory-prod.py \
+  -e administrator_password="$ADMINISTRATOR_PASSWORD" \
+  -e force_reinstall=false \
+  install-playbook.yaml
